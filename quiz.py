e82ae0d76c2cc4708ec29fd900fdc7c870ac8d64
Aditi Bhandari <aditibhandari24@gmail.com>
2014-09-07 03:59:47 -0400

    Update quiz.py
    added for more info

--- quiz.py
+++ quiz.py
@@ -13,9 +13,9 @@ Q = ["What is a non metal that remains liquid at room temperature? ",
      "What is used as a moderator in a nuclear reactor? ",
      "Isotopes are separated by: ",
      "What radioactive pollutant has drawn public attention, due to its occurrence in the building material? ",
-     "Who suggested that most of the mass in the atom is located in the nucleus? ",
+     "Who suggested that most of the mass in the atom is located in the nucleus ?",
      "According to Avogadro's Hypothesis, the smallest particle of an element or a compound, that can exist independently, is called a(n) ",
-     "Nuclear fission is caused by the impact of ",
+     "Nuclear fission is caused by the impact of: ",
      "How many colors are in the sunlight spectrum? ",
      "What color is bromine? ",
      "The hardest substance available on earth is: ",
@@ -39,6 +39,25 @@ A = ["bromine",
      "peat",
      "water",
      "j"]
+     
+moreinfo = ["http://www.softschools.com/facts/periodic_table/bromine_facts/213/",
+            "http://scifun.chem.wisc.edu/chemweek/chlrphyl/chlrphyl.html",
+            "http://www.sciencekids.co.nz/sciencefacts/chemistry/carbon.html",
+            "http://www.ducksters.com/science/chemistry/mercury.php",
+            "http://www.ducksters.com/science/chemistry/nitrogen.php",
+            "http://www.chemicool.com/elements/radium.html",
+            "http://stilltutorial.com/2013/08/distilling-10-interesting-things-you-didnt-know/",
+            "http://www.chemicool.com/elements/thorium.html",
+            "http://ernestrutherfordpd7.weebly.com/interesting-facts.html",
+            "http://www.ducksters.com/science/molecules.php",
+            "http://scienceforkids.kidipede.com/chemistry/atoms/neutron.htm",
+            "http://www.sciencekids.co.nz/sciencefacts/light.html",
+            "http://www.softschools.com/facts/periodic_table/bromine_facts/213/",
+            "http://www.softschools.com/facts/periodic_table/carbon_facts/183/",
+            "http://www.snsk.no/facts-about-coal.145744.en.html",
+            "http://www.sciencekids.co.nz/sciencefacts/water.html",
+            "http://www.ptable.com/"]
+            
 
 i = randint(0, 14)
 answer = raw_input("Question 1:" + Q[i])
@@ -47,6 +66,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 
 i = randint(0, 14) 
@@ -56,6 +76,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14)
 answer = raw_input("\nQuestion 3:" + Q[i])
@@ -64,6 +85,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
 answer = raw_input("\nQuestion 4:" + Q[i])
@@ -72,6 +94,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14)
 answer = raw_input("\nQuestion 5:" + Q[i])
@@ -80,6 +103,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
 answer = raw_input("\nQuestion 6:" + Q[i])
@@ -88,6 +112,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14)
 answer = raw_input("\nQuestion 7:" + Q[i])
@@ -96,6 +121,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
 answer = raw_input("\nQuestion 8:" + Q[i])
@@ -104,6 +130,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14)
 answer = raw_input("\nQuestion 9:" + Q[i])
@@ -112,6 +139,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
 answer = raw_input("\nQuestion 10:" + Q[i])
@@ -120,6 +148,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14)
 answer = raw_input("\nQuestion 11:" + Q[i])
@@ -128,6 +157,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
 answer = raw_input("\nQuestion 12:" + Q[i])
@@ -136,6 +166,7 @@ if answer.lower() == A[i]:
     count = count + 1
 else:
     print "Wrong \nCorrect Answer : " + A[i]
+    print "To learn more visit this site" + moreinfo[i]
 
 
 print "\nOut of 14 questions asked, you got %d of them right." % (count)
